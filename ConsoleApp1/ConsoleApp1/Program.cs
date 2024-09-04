b9ef57adb0137af3ffbac9116de4b5a79fd1a02f
KonstantineGvritishvili <kotegvriti1@gmail.com>
2024-09-04 04:33:53 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle car = null;
+            List<Vehicle> carList = new List<Vehicle>();
+
+            while (true)
+            {
+                Console.WriteLine("\n Vehicle Information:");
+                Console.WriteLine("1. Register Car");
+                Console.WriteLine("2. Show Car");
+                Console.WriteLine("3. Drive Car");
+                Console.WriteLine("4. Refuel car");
+                Console.WriteLine("5. exit");
+                Console.Write("write choose: ");
+
+                int makeChoice = int.Parse(Console.ReadLine());
+
+                switch (makeChoice)
+                {
+                    case 1:
+                        Console.WriteLine("\n1. Register Mercedes");
+                        Console.WriteLine("2. Register BMW");
+
+                        int choiceBetweenCars = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nEnter Name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("\nEnter Fuel:");
+                        int fuel = int.Parse(Console.ReadLine());
+
+                        if (choiceBetweenCars == 1)
+                        {
+                            car = new Vehicle(name, fuel);
+                            carList.Add(car);
+                        }
+                        else if (choiceBetweenCars == 2)
+                        {
+                            car = new Vehicle(name, fuel);
+                            carList.Add(car);
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\n Car List:");
+
+                        foreach (var item in carList)
+                        {
+                            Console.WriteLine($"Name: {item.Name}, Fuel: {item.Fuel}");
+                        }
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nSelect a car to drive:");
+
+                        for (int i = 0; i < carList.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {carList[i].Name}");
+                        }
+
+                        int choice = int.Parse(Console.ReadLine());
+
+                        if (choice > 0 && choice <= carList.Count)
+                        {
+                            carList[choice - 1].Drive();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No");
+                        }
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\n Select a car to refuel:");
+
+                        for (int i = 0; i < carList.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {carList[i].Name}");
+                        }
+
+                        int choiceRefuel = int.Parse(Console.ReadLine());
+
+                        if (choiceRefuel > 0 && choiceRefuel <= carList.Count)
+                        {
+                            Console.WriteLine("Enter amount to refuel:");
+                            int refuelAmount = int.Parse(Console.ReadLine());
+                            carList[choiceRefuel - 1].Refuel(refuelAmount);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No");
+                        }
+
+                        break;
+
+                    case 5:
+                        return;
+
+
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Vehicle 
+    {
+
+        public string Name { get; set; }
+        public int Fuel { get; set; }
+
+        public Vehicle(string name, int fuel)
+        {
+            Name = name;
+            Fuel = fuel;
+        }
+
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine($"{Name} is driving.");
+                Fuel-= 10;
+                Console.WriteLine($"Fuel left: {Fuel}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} has no fuel.");
+            }
+
+            if(Fuel < 0)
+            {
+                Console.WriteLine($"You cannot drive on {Name} anymore its have not fuel, you must refuel car");
+            }
+        }
+
+        public void Refuel(int amount)
+        {
+            Fuel += amount;
+            Console.WriteLine($"Refueled {Name} with {amount} units of fuel.");
+            Console.WriteLine($"Fuel left: {Fuel}");
+
+            if (amount > 100)
+            {
+                Console.WriteLine($"You cannot refuel {Name} with this amount of fuel.");
+                return;
+            }
+        }
+
+    }
+}
